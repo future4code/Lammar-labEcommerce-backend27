8539150d959919ce2268de5f86ff0b116d0eb47a
Mauricio da Rosa Gonçalves <mauriciorg89@hotmail.com>
2022-12-03 09:42:09 -0300

    filtro e ordenação prontos

--- src/endpoints/getAllCharacters.ts
+++ src/endpoints/getAllCharacters.ts
@@ -6,10 +6,14 @@ export default async function getAllCharacters(
     res: Response
 ):Promise<void> {
    try {
-    const name = req.query
+    const {name, orderBy, orderType, page} = req.query
+    const resultsPerPage = 5
+    const offset = resultsPerPage * (Number(page)-1)
 
     const characters: character[] = await connection("character")
-
+      .where("name", "LIKE", `%${name}%`)
+      .orderBy(orderBy as string || "name", orderType as string)
+    .offset(offset)
 
 
     res.send(characters)
